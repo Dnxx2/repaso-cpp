b22159b20a74548029c2478ceeb52ccd6932db9f
Daniel <28082006dm@gmail.com>
2026-09-03 12:02:10 -0600

    Ejercicio 1 repaso

---
+++ r00-compilacion/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "suma.h"
+using namespace std;
+
+int main(){
+  cout<<sumar(15,12)<<"\n";
+  
+  return 0;
+}
---
+++ r00-compilacion/suma.cpp
@@ -0,0 +1,3 @@
+#include "suma.h"
+
+int sumar(int a, int b){return a+b;}
