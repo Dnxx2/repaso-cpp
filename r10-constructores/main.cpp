79996b2438325d2de9b8bfd55775e3f119246a13
Daniel <28082006dm@gmail.com>
2026-09-06 17:18:15 -0600

    Repaso 10 constructores

---
+++ r10-constructores/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+Sesion global("global");  // constructores y destructor imprimen
+int main() {
+    Sesion a("a");
+    { Sesion b("b"); Sesion c("c", 2); }  /*Sesion(global)
+                                             Sesion(a)
+                                            Sesion(b)
+                                           Sesion(c, 2)
+                                           ~Sesion(c)*/
+    std::cout << "--- medio ---\n";
+    Sesion d;
+}
